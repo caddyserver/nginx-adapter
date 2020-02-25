69bbd5ef3f5d4566354f20189733062afdad4d88
Mohammed Al Sahaf <msaa1990@gmail.com>
2020-02-25 23:17:42 +0300

    improve the build script

--- build_caddy.sh
+++ build_caddy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-mkdir caddy || true
-cd caddy || true
+set -euxo pipefail
+
+# Get the Caddy version currenly used for the adapter
+CADDY_VERSION=$(go list -m github.com/caddyserver/caddy/v2 | cut -d " " -f 2)
+
+mkdir -p caddy
+cd caddy
 curl "https://raw.githubusercontent.com/caddyserver/caddy/v2/cmd/caddy/main.go" > main.go
 sed -i.bak 's/^)/	_ "github.com\/caddyserver\/nginx-adapter"\'$'\n)/' main.go && rm main.go.bak
 
 go mod init caddy
-grep 'require' ../go.mod >> go.mod
-go build
+go mod edit -require=github.com/caddyserver/caddy/v2@"$CADDY_VERSION"
+go build -o caddy_v2
